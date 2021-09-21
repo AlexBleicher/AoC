e30768300341677820a7f91c65d3715e128c9d90
AlexBleicher <alexander.bleicher01@gmail.com>
2021-09-21 09:35:08 +0200

    AoC2019 Day 2 Task 2

--- src/main/java/AoC2019/Day2/Day2.java
+++ src/main/java/AoC2019/Day2/Day2.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 public class Day2 {
 
     private ArrayList<Integer> intProgram;
+    private final ArrayList<Integer> memory;
 
     public Day2(String stringInput) {
         intProgram = new Input(stringInput).transformInput();
+        memory = new Input(stringInput).transformInput();
     }
 
     public ArrayList<Integer> runIntProgram() {
@@ -20,7 +22,7 @@ public class Day2 {
             } else if (optcode == 2) {
                 intProgram.set(resultPosition, intProgram.get(intProgram.get(index + 1)) * intProgram.get(intProgram.get(index + 2)));
             } else {
-                throw new RuntimeException("Diese Zahl sollte hier nicht stehen!");
+                throw new RuntimeException(optcode + "Diese Zahl sollte hier nicht stehen!");
             }
             index += 4;
             optcode = intProgram.get(index);
@@ -32,5 +34,22 @@ public class Day2 {
         intProgram.set(1, 12);
         intProgram.set(2, 2);
         return runIntProgram().get(0);
+    }
+
+    public int solveTask2() {
+        for (int noun = 0; noun <= 99; noun++) {
+            for (int verb = 0; verb <= 99; verb++) {
+                intProgram.set(1, noun);
+                intProgram.set(2, verb);
+                if (runIntProgram().get(0) == 19690720) {
+                    return (100 * noun + verb);
+                } else {
+                    for (int i = 0; i < intProgram.size(); i++) {
+                        intProgram.set(i, memory.get(i));
+                    }
+                }
+            }
+        }
+        return 0;
     }
 }
