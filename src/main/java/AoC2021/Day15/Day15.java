62e87c5b350201a320ac0e24d42ad597762663f5
AlexBleicher <alexander.bleicher@gmail.com>
2021-12-15 17:55:43 +0100

    AoC2019 Day 15 Dijekstra

--- src/main/java/AoC2021/Day15/Day15.java
+++ src/main/java/AoC2021/Day15/Day15.java
@@ -12,22 +12,50 @@ public class Day15 {
     }
 
     public int task1() {
-        return lowestRiskPath();
+        return lowestRiskPath(riskLevelList.get(0));
     }
 
-    public int lowestRiskPath(){
-        RiskLevel bottom = riskLevelList.get(riskLevelList.size()-1);
-        List<List<RiskLevel>> allPaths = riskLevelList.get(0).getAllPathsToEnd(bottom);
-        int min =-1;
-        for (List<RiskLevel> path : allPaths) {
-            int scoreOfPath =0;
-            for (RiskLevel riskLevel : path) {
-                scoreOfPath+=riskLevel.getValue();
-            }
-            if(min==-1||min>scoreOfPath){
-                min = scoreOfPath;
+    public int lowestRiskPath(RiskLevel start) {
+        List<RiskLevel> queue = new ArrayList<>();
+        List<Integer> distance = new ArrayList<>();
+        List<RiskLevel> path = new ArrayList<>();
+        initialise(queue, distance, path, start);
+        while (!queue.isEmpty()) {
+            RiskLevel u = getRiskLevelWithLowestDistance(distance, queue);
+            queue.remove(u);
+            for (RiskLevel neighbor : u.getNeighbors()) {
+                if (queue.contains(neighbor)) {
+                    update_Distance(u, neighbor, distance, path);
+                }
             }
         }
-        return min;
+        return distance.get(distance.size() - 1);
+    }
+
+    public void initialise(List<RiskLevel> queue, List<Integer> distance, List<RiskLevel> path, RiskLevel start) {
+        for (RiskLevel riskLevel : riskLevelList) {
+            distance.add(1000000000);
+            path.add(null);
+        }
+        distance.set(riskLevelList.indexOf(start), 0);
+        queue.addAll(riskLevelList);
+    }
+
+    public void update_Distance(RiskLevel u, RiskLevel v, List<Integer> distance, List<RiskLevel> path) {
+        int alternative = distance.get(riskLevelList.indexOf(u)) + u.getValue();
+        if (alternative < distance.get(riskLevelList.indexOf(v))) {
+            distance.set(riskLevelList.indexOf(v), alternative);
+            path.set(riskLevelList.indexOf(v), u);
+        }
+    }
+
+    public RiskLevel getRiskLevelWithLowestDistance(List<Integer> distance, List<RiskLevel> queue) {
+        int lowest = -1;
+        for (Integer thisDistance : distance) {
+            if (lowest == -1 || (lowest > thisDistance)) {
+                lowest = thisDistance;
+            }
+        }
+        return queue.get(distance.indexOf(lowest));
     }
 }
--- src/main/java/AoC2021/Day15/RiskLevel.java
+++ src/main/java/AoC2021/Day15/RiskLevel.java
@@ -17,7 +17,7 @@ public class RiskLevel {
     }
 
     public void addNeighbor(RiskLevel riskLevel) {
-        if (!neighbors.contains(riskLevel) && ((riskLevel.getX() == x + 1) && (riskLevel.getY() == y) || (riskLevel.getY() == y + 1 && riskLevel.getX() == x))) {
+        if (!neighbors.contains(riskLevel) && ((riskLevel.getX() == x + 1 || riskLevel.getX() == x - 1) && (riskLevel.getY() == y) || ((riskLevel.getY() == y + 1 || riskLevel.getY() == y - 1) && riskLevel.getX() == x))) {
             neighbors.add(riskLevel);
         }
     }
@@ -34,17 +34,7 @@ public class RiskLevel {
         return value;
     }
 
-    public List<List<RiskLevel>> getAllPathsToEnd(RiskLevel bottom) {
-        List<List<RiskLevel>> allPaths = new ArrayList<>();
-        if (!bottom.equals(this)) {
-            for (RiskLevel neighbor : neighbors) {
-                List<List<RiskLevel>> allPathsOfNeighbor = neighbor.getAllPathsToEnd(bottom);
-                for (List<RiskLevel> path : allPathsOfNeighbor) {
-                    path.add(this);
-                }
-                allPaths.addAll(allPathsOfNeighbor);
-            }
-        }
-        return allPaths;
+    public List<RiskLevel> getNeighbors() {
+        return neighbors;
     }
 }
