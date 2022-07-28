9c461e1910830ac59d8298f435604545ed96d1bc
AlexBleicher <alexander.bleicher01@gmail.com>
2022-07-28 12:46:14 +0200

    AoC 2021 Day 15 Task2 (Runtime not great. But not terrible either)

--- src/main/java/AoC2021/Day15/Input.java
+++ src/main/java/AoC2021/Day15/Input.java
@@ -14,14 +14,37 @@ public class Input {
     public List<RiskLevel> parseInput() {
         List<RiskLevel> returnValue = new ArrayList<>();
         String[] inputLines = input.split("\n");
+        int lineLength = inputLines[0].length();
         int y = 0;
         for (String inputLine : inputLines) {
             for (int x = 0; x < inputLine.length(); x++) {
-                RiskLevel newRiskLevel = new RiskLevel(x, y, Integer.parseInt("" + inputLine.charAt(x)));
+                int risk = Integer.parseInt("" + inputLine.charAt(x));
+                RiskLevel newRiskLevel = new RiskLevel(x, y, risk);
                 returnValue.add(newRiskLevel);
+                for (int i = 1; i < 5; i++) {
+                    risk += 1;
+                    if (risk == 10) {
+                        risk = 1;
+                    }
+                    RiskLevel riskLevelToAdd = new RiskLevel((x + i * lineLength), y, risk);
+                    returnValue.add(riskLevelToAdd);
+                }
             }
             y++;
         }
+        List<RiskLevel> risksToAdd = new ArrayList<>();
+        for (RiskLevel riskLevel : returnValue) {
+            int risk = riskLevel.getRisk();
+            for (int i = 1; i < 5; i++) {
+                risk += 1;
+                if (risk == 10) {
+                    risk = 1;
+                }
+                RiskLevel riskLevelToAdd = new RiskLevel(riskLevel.getX(), riskLevel.getY() + i * lineLength, risk);
+                risksToAdd.add(riskLevelToAdd);
+            }
+        }
+        returnValue.addAll(risksToAdd);
         for (RiskLevel riskLevel : returnValue) {
             for (RiskLevel possibleNeighbor : returnValue) {
                 if (((possibleNeighbor.getX() == riskLevel.getX() - 1 || possibleNeighbor.getX() == riskLevel.getX() + 1) && possibleNeighbor.getY() == riskLevel.getY()) ||
