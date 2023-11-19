4ebb62831b631663344457e03c8b1cd0c3d4ab6a
bleicheral88417 <bleicheral88417@th-nuernberg.de>
2023-11-19 13:27:56 +0100

    AoC 2022 day 13 task 2 solved

--- src/main/java/AoC2022/Day13/Day13.java
+++ src/main/java/AoC2022/Day13/Day13.java
@@ -1,5 +1,6 @@
 package AoC2022.Day13;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day13 {
@@ -13,10 +14,62 @@ public class Day13 {
     public int solveTask1() {
         int sum = 0;
         for (int i = 0; i < pairs.size(); i++) {
-            if (pairs.get(i).isRightOrder()) {
+            if (compare(pairs.get(i).getLeft().items, pairs.get(i).getRight().items) < 0) {
                 sum += i + 1;
             }
         }
         return sum;
+    }
+
+    public int solveTask2() {
+        List<Packet> allPackets = new ArrayList<>();
+        for (Pair pair : pairs) {
+            allPackets.add(pair.getLeft());
+            allPackets.add(pair.getRight());
+        }
+
+        List<Packet> dividers = List.of(new Packet("[[2]]"), new Packet("[[6]]"));
+        allPackets.addAll(dividers);
+        List<List<Object>> sortedPackets = allPackets.stream()
+                .map(packet -> packet.items)
+                .sorted(this::compare).toList();
+
+        System.out.println(sortedPackets);
+        List<List<Object>> dividersAsItems = dividers.stream().map(packet -> packet.items).toList();
+        Integer res = dividersAsItems.stream()
+                .map(sortedPackets::indexOf)
+                .reduce((a, b) -> (a + 1) * (b + 1))
+                .orElseThrow();
+
+        return res;
+    }
+
+    private int compare(Object leftItem, Object rightItem) {
+        if (leftItem instanceof Integer leftInt && rightItem instanceof Integer rightInt) {
+            return leftInt - rightInt;
+        }
+
+        if (leftItem instanceof List<?> leftList && rightItem instanceof List<?> rightList) {
+            if (leftList.isEmpty()) {
+                return rightList.isEmpty() ? 0 : -1;
+            }
+            int minSize = Math.min(leftList.size(), rightList.size());
+            for (int i = 0; i < minSize; i++) {
+                int compareRes = compare(leftList.get(i), rightList.get(i));
+                if (compareRes != 0) {
+                    return compareRes;
+                }
+            }
+            return leftList.size() - rightList.size();
+        }
+
+        if (leftItem instanceof Integer && rightItem instanceof List<?>) {
+            return compare(List.of(leftItem), rightItem);
+        }
+        if (leftItem instanceof List<?> && rightItem instanceof Integer) {
+            return compare(leftItem, List.of(rightItem));
+        }
+
+        throw new RuntimeException("Somethings not right here");
     }
 }
--- src/main/java/AoC2022/Day13/Pair.java
+++ src/main/java/AoC2022/Day13/Pair.java
@@ -1,7 +1,5 @@
 package AoC2022.Day13;
 
-import java.util.List;
-
 public class Pair {
 
     private Packet left;
@@ -12,36 +10,11 @@ public class Pair {
         right = new Packet(rightString);
     }
 
-    public boolean isRightOrder() {
-        return compare(left.items, right.items) < 0;
+    public Packet getLeft() {
+        return left;
     }
 
-    private int compare(Object leftItem, Object rightItem) {
-        if (leftItem instanceof Integer leftInt && rightItem instanceof Integer rightInt) {
-            return leftInt - rightInt;
-        }
-
-        if (leftItem instanceof List<?> leftList && rightItem instanceof List<?> rightList) {
-            if (leftList.isEmpty()) {
-                return rightList.isEmpty() ? 0 : -1;
-            }
-            int minSize = Math.min(leftList.size(), rightList.size());
-            for (int i = 0; i < minSize; i++) {
-                int compareRes = compare(leftList.get(i), rightList.get(i));
-                if (compareRes != 0) {
-                    return compareRes;
-                }
-            }
-            return leftList.size() - rightList.size();
-        }
-
-        if (leftItem instanceof Integer && rightItem instanceof List<?>) {
-            return compare(List.of(leftItem), rightItem);
-        }
-        if (leftItem instanceof List<?> && rightItem instanceof Integer) {
-            return compare(leftItem, List.of(rightItem));
-        }
-
-        throw new RuntimeException("Somethings not right here");
+    public Packet getRight() {
+        return right;
     }
 }
